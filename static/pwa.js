8624f8ae61ca41ee90fadf85e4c66ca9b6cd1cc5
AH Web Solutions <39820963+redirectpages@users.noreply.github.com>
2023-11-16 11:29:03 +0630

    Update pwa.js

--- static/pwa.js
+++ static/pwa.js
@@ -1,18 +1,9 @@
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/sw.js', { scope: '/' })
-        .then((registration) => {
-            if (registration.active) {
-                const data = {
-                    type: 'CACHE_URLS',
-                    payload: [
-                        location.href,
-                        ...performance.getEntriesByType('resource').map((r) => r.name)
-                    ]
-                };
-                registration.active.postMessage(data);
-            } else {
-                console.log('No active service worker available');
-            }
-        })
-        .catch((err) => console.log('SW registration FAIL:', err));
+    window.addEventListener('load', function() {
+        navigator.serviceWorker.register('/sw.js').then(function(registration) {
+        console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function(err) {
+        console.log('ServiceWorker registration failed: ', err);
+        });
+    });
 }
