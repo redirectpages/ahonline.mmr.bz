7054f335f071d31af10ff18d86b7d8f2bd605388
AH Web Solutions <39820963+redirectpages@users.noreply.github.com>
2023-11-16 11:11:35 +0630

    Update sw.js

--- static/sw.js
+++ static/sw.js
@@ -23,49 +23,31 @@ if (workbox.navigationPreload.isSupported()) {
 
 self.addEventListener('fetch', (event) => {
   if (event.request.mode === 'navigate') {
-    event.respondWith((async () => {
-      try {
-        const cachedResp = await fromCache(event.request);
-        if (cachedResp) {
-          return cachedResp;
+    event.respondWith(
+      (async () => {
+        try {
+          const cachedResp = await caches.match(event.request);
+          if (cachedResp) {
+            return cachedResp;
+          }
+        } catch (error) {
+          // Handle cache errors, if needed
         }
-      } catch (error) {
-        // Handle cache errors, if needed
-      }
 
-      try {
-        const networkResp = await fromNetwork(event.request, 5000);
-        if (networkResp && networkResp.status === 200) {
-          event.waitUntil(update(event.request.clone(), networkResp.clone()));
-          return networkResp;
+        try {
+          const networkResp = await fetch(event.request);
+          if (networkResp && networkResp.status === 200) {
+            const cache = await caches.open(CACHE_NAME);
+            cache.put(event.request, networkResp.clone());
+            return networkResp;
+          }
+        } catch (error) {
+          // Handle network errors, if needed
         }
-      } catch (error) {
-        // Handle network errors, if needed
-      }
-
-      // If both cache and network fail, respond with a generic offline message
-      return new Response('Offline', { status: 503, statusText: 'Service Unavailable' });
-    })());
+          
+        return new Response('Offline', { status: 503, statusText: 'Service Unavailable' });
+      })()
+    );
   }
 });
 
-const update = (request, response) =>
-  caches.open(KEY).then((cache) => cache.put(request, response));
-
-const fromNetwork = (request, timeout) =>
-  new Promise((fulfill, reject) => {
-    const timeoutId = setTimeout(reject, timeout);
-    fetch(request)
-      .then((response) => {
-        clearTimeout(timeoutId);
-        fulfill(response);
-      })
-      .catch(reject);
-  });
-
-const fromCache = (request) =>
-  caches
-    .open(KEY)
-    .then((cache) =>
-      cache.match(request).then((matching) => matching || cache.match('/'))
-    );
