8be81874d5d5c4ad942f2978d4dd91202cef2161
Aung Phyo Pyae Kyaw <aungphyopyaekyaw@gmail.com>
2023-11-21 19:17:09 +0630

    www to non-www

--- static/pwa.js
+++ static/pwa.js
@@ -1,3 +1,6 @@
+if (window.location.hostname.indexOf("www") == 0) {
+    window.location = window.location.href.replace("www.","");
+}
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', function() {
         navigator.serviceWorker.register('/sw.js').then(function(registration) {
